e5d367f29c66bee932c1349b447e4a2a9d848d08
AnnaTRocha <95356877+AnnaTRocha@users.noreply.github.com>
2022-06-11 18:36:35 -0300

    Escrita
    Ajustado aparência do teclado, e agora dá de fazer tentativas.

--- script.js
+++ script.js
@@ -14,29 +14,100 @@ const columns = 5
 let currentRow = 0
 let currentColumn = 0
 
+const letreco = "VASCO";
+let letrecoMap = {}
+for(let index = 0; index < letreco.length; index++){
+    letrecoMap[letreco[index]] = index;
+}
+
+const guesses = []
+
 for (let rowIndex = 0; rowIndex < rows; rowIndex++) {
-    const tileRow = document.createElement("div")
-    tileRow.setAttribute("id", "row"+rowIndex)
-    tileRow.setAttribute("class", "tile-row")
+    //salvando as tentativas
+    guesses[rowIndex] = new Array(columns);
+
+    const tileRow = document.createElement("div");
+    tileRow.setAttribute("id", "row"+rowIndex);
+    tileRow.setAttribute("class", "tile-row");
+    
+    //Coloca um ID para cada bloquinho(linha e coluna)
+
     for (let columnIndex = 0; columnIndex < columns; columnIndex++) {
-        const tileColumn = document.createElement("div")
-        tileColumn.setAttribute("id", "row"+rowIndex+"column"+columnIndex)
-        tileColumn.setAttribute("class", "tile-column")
-        tileRow.append(tileColumn)
+        const tileColumn = document.createElement("div");
+        tileColumn.setAttribute("id", "row"+rowIndex+"column"+columnIndex);
+        //caso foi a primeira fileira, é o typing, caso contrário disable
+        tileColumn.setAttribute("class", rowIndex === 0 ? "tile-column typing" : "tile-column disabled");
+        tileRow.append(tileColumn);
+        guesses[rowIndex][columnIndex] = "";
     }
-    tiles.append(tileRow)
+    tiles.append(tileRow);
     
 }
 
+const checkGuess = () =>{
+    const guess = guesses[currentRow].join("") // juntando letras, sem espaços
+    if(guess.length !== columns){
+        return;
+    }
+
+    var currentColumns = document.querySelectorAll(".typing");
+
+    for (let index = 0; index < columns; index++) {
+        const letter = guess[index];
+
+        if(letrecoMap[letter] === undefined){
+            currentColumns[index].classList.add("wrong");
+        } else { 
+            if(letrecoMap[letter] === index){
+                //letter find, in correct place
+                currentColumns[index].classList.add("right");
+            } else{
+                //letter find, but in wrong place
+                currentColumns[index].classList.add("displaced");
+            }
+        }
+        
+    }
+
+    if(guess === letreco){
+        window.alert("Parabéns, não fez mais que sua obrigação.");
+    } 
+    if (currentRow === rows - 1) {
+        window.alert("Burro");
+    } else {
+        moveToNextRow();
+    }
+};
+
+const moveToNextRow = () => {
+    currentRow++;
+    currentColumn = 0;
+}
+
+const handleKeybordOnClick = (key) => {
+    //Como cada bloquinho foi adicionado um ID anteriomente, poderemos acessa-lo aqui
+    if(currentColumn === columns){
+        return
+    }
+    
+    const currentTile = document.querySelector(
+        "#row"+currentRow+"column"+currentColumn
+    );
+    currentTile.textContent = key;
+    //coloca as letras na matriz
+    guesses[currentRow][currentColumn] = key
+    currentColumn++;
+};
+
 const createKeybordRow = (keys, keybordRow) =>{
     keys.forEach((key) => {
-        var buttonElement = document.createElement("button")
-        buttonElement.textContent = key
-        buttonElement.setAttribute("id", key)
-        buttonElement.addEventListener("click", () => handleKeybordOnClick)
-        keybordRow.append(buttonElement)
-    })
-}
+        var buttonElement = document.createElement("button");
+        buttonElement.textContent = key;
+        buttonElement.setAttribute("id", key);
+        buttonElement.addEventListener("click", () => handleKeybordOnClick(key));
+        keybordRow.append(buttonElement);
+    });
+};
 
 createKeybordRow(keysFirstRow, keybordFirstRow)
 createKeybordRow(keysSecondRow, keybordSecondRow)
@@ -51,16 +122,12 @@ backspaceButton.addEventListener("click", handleBackspace)
 backspaceButton.textContent = "<"
 backspaceAndEnterRow.append(backspaceButton)
 
-const handleEnter = () =>{
-    console.log("verifica")
-} 
-
 const enterButton = document.createElement("button")
-enterButton.addEventListener("click", handleEnter)
+enterButton.addEventListener("click", checkGuess)
 enterButton.textContent = "ENTER"
 backspaceAndEnterRow.append(enterButton)
 
-const preferences = document.createElement("button")
-preferences.addEventListener("click", handleEnter)
-preferences.textContent = "Preferencias"
-backspaceAndEnterRow.append(preferences)
+const preferences = document.createElement("button");
+preferences.addEventListener("click", checkGuess);
+preferences.textContent = "Preferencias";
+backspaceAndEnterRow.append(preferences);
